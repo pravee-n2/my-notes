b9758c2533770fc9b2e0b6afb4fa7fa4ad39aaab
pravee-n2 <moulishnavee2000@gmail.com>
2026-02-13 09:55:54 +0530

    fix base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "pravee-n2.github.io/my-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
